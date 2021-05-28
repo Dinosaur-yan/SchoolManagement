af3c0c77cc6caf1378f86725a92ae135dcdd817e
lcx <486035085@qq.com>
2021-05-28 17:36:47 +0800

    编辑学生信息

--- src/SchoolManagement/Controllers/HomeController.cs
+++ src/SchoolManagement/Controllers/HomeController.cs
@@ -81,5 +81,78 @@ namespace SchoolManagement.Controllers
 
             return View();
         }
+
+        [HttpGet]
+        public ViewResult Edit(int id)
+        {
+            Student student = _studentRepository.GetStudentById(id);
+            StudentEditViewModel studentEditViewModel = new()
+            {
+                Id = student.Id,
+                Name = student.Name,
+                Email = student.Email,
+                Major = student.Major,
+                ExistingPhotoPath = student.PhotoPath
+            };
+            return View(studentEditViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(StudentEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Student student = _studentRepository.GetStudentById(model.Id);
+
+                student.Name = model.Name;
+                student.Email = model.Email;
+                student.Major = model.Major;
+                student.PhotoPath = ProcessUploadedFile(null, model.Photos);
+
+                Student updatedstudent = _studentRepository.Update(student);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(model);
+        }
+
+        private string ProcessUploadedFile(string originalPath, List<IFormFile> files)
+        {
+            if (files == null || !files.Any()) return originalPath;
+
+            try
+            {
+                string uniqueFileName = null;
+                foreach (IFormFile file in files)
+                {
+                    string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images", "avatars");
+                    if (!Directory.Exists(uploadsFolder))
+                    {
+                        Directory.CreateDirectory(uploadsFolder);
+                    }
+                    uniqueFileName = Guid.NewGuid().ToString("N") + "_" + file.FileName;
+                    string newFilePath = Path.Combine(uploadsFolder, uniqueFileName);
+                    using (var fileStream = new FileStream(newFilePath, FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+                }
+
+                if (!string.IsNullOrWhiteSpace(originalPath))
+                    return uniqueFileName;
+
+                string filePath = Path.Combine(_webHostEnvironment.WebRootPath, "images", "avatars", originalPath ?? "");
+                if (System.IO.File.Exists(filePath))
+                {
+                    System.IO.File.Delete(filePath);
+                }
+
+                return uniqueFileName;
+            }
+            catch
+            {
+                return null;
+            }
+        }
     }
 }
