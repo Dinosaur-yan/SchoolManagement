22b4bb9257c80041b3d92f1832eeab2711067cd7
lcx <486035085@qq.com>
2021-05-31 16:16:12 +0800

    异常页面加载

---
+++ src/SchoolManagement/Controllers/ErrorController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolManagement.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("error")]
+        public IActionResult Error()
+        {
+            var exceptionHandlerPathFeature = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            ViewBag.ExceptionPath = ((ExceptionHandlerFeature)exceptionHandlerPathFeature).Path;
+            ViewBag.ErrorMessage = exceptionHandlerPathFeature.Error.Message;
+            ViewBag.StackTrace = exceptionHandlerPathFeature.Error.StackTrace;
+            return View("Error");
+        }
+
+        [Route("error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            var statusCodeResult = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+
+            switch (statusCode)
+            {
+                case StatusCodes.Status404NotFound:
+                default:
+                    ViewBag.ErrorMessage = "抱歉，用户访问的页面不存在";
+                    ViewBag.Path = statusCodeResult.OriginalPath;
+                    ViewBag.QueryString = statusCodeResult.OriginalQueryString;
+                    return View("NotFound");
+            }
+        }
+    }
+}
--- src/SchoolManagement/Startup.cs
+++ src/SchoolManagement/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -40,6 +40,11 @@ namespace SchoolManagement
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else if (env.IsStaging() || env.IsProduction() || env.IsEnvironment("UAT"))
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseStatusCodePagesWithReExecute("/Error/{0}");  //{0}占位符，会自动接收http中的状态码
             }
 
             app.UseStaticFiles();
