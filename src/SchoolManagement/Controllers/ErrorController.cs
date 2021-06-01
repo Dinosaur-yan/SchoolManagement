0348eb436bd3fd999a142bd7a89456b6ac8cb50a
lcx <486035085@qq.com>
2021-06-01 15:03:41 +0800

    添加nlog记录日志

--- src/SchoolManagement/Controllers/ErrorController.cs
+++ src/SchoolManagement/Controllers/ErrorController.cs
@@ -1,19 +1,29 @@
 ﻿using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace SchoolManagement.Controllers
 {
     public class ErrorController : Controller
     {
+        private readonly ILogger<ErrorController> _logger;
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            _logger = logger;
+        }
+
         [Route("error")]
         public IActionResult Error()
         {
             var exceptionHandlerPathFeature = HttpContext.Features.Get<IExceptionHandlerFeature>();
 
-            ViewBag.ExceptionPath = ((ExceptionHandlerFeature)exceptionHandlerPathFeature).Path;
-            ViewBag.ErrorMessage = exceptionHandlerPathFeature.Error.Message;
-            ViewBag.StackTrace = exceptionHandlerPathFeature.Error.StackTrace;
+            //ViewBag.ExceptionPath = ((ExceptionHandlerFeature)exceptionHandlerPathFeature).Path;
+            //ViewBag.ErrorMessage = exceptionHandlerPathFeature.Error.Message;
+            //ViewBag.StackTrace = exceptionHandlerPathFeature.Error.StackTrace;
+
+            _logger.LogError($"路径{((ExceptionHandlerFeature)exceptionHandlerPathFeature).Path}，产生了一个错误：{exceptionHandlerPathFeature.Error.Message}");
+
             return View("Error");
         }
 
@@ -22,13 +32,16 @@ namespace SchoolManagement.Controllers
         {
             var statusCodeResult = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
 
+            //ViewBag.Path = statusCodeResult.OriginalPath;
+            //ViewBag.QueryString = statusCodeResult.OriginalQueryString;
+
             switch (statusCode)
             {
                 case StatusCodes.Status404NotFound:
                 default:
                     ViewBag.ErrorMessage = "抱歉，用户访问的页面不存在";
-                    ViewBag.Path = statusCodeResult.OriginalPath;
-                    ViewBag.QueryString = statusCodeResult.OriginalQueryString;
+
+                    _logger.LogError($"发生了一个404错误，路径{statusCodeResult.OriginalPath + statusCodeResult.OriginalQueryString}");
                     return View("NotFound");
             }
         }
--- src/SchoolManagement/Program.cs
+++ src/SchoolManagement/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+using System;
 
 namespace SchoolManagement
 {
@@ -7,9 +10,23 @@ namespace SchoolManagement
     {
         public static void Main(string[] args)
         {
-            IHostBuilder hostBuilder = CreateHostBuilder(args);
-            IHost host = hostBuilder.Build();
-            host.Run();
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Info("initial main");
+                IHostBuilder hostBuilder = CreateHostBuilder(args);
+                IHost host = hostBuilder.Build();
+                host.Run();
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -17,6 +34,12 @@ namespace SchoolManagement
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                })
+                .UseNLog();
     }
 }
